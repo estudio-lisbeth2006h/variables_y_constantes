9dad0a8b1beeeb8f3812f05d308e4962c1ae0a26
Lisbeth Herrera Berrio <lisbeth.herrera@GRUPOBLUMAN.INTRANET>
2024-04-12 15:33:13 -0500

    Subiendo el index y el ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,8 @@
+console.log
+
+var numero1 = parseInt(prompt("Introduce un número"));
+var numero2 = prompt("Introduce otro número");
+
+var resultado = numero1 + parseInt(numero2);
+
+alert("La suma entre  " + numero1 + " y "+ numero2 + " es " + resultado);
