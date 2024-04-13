fa0d168a4dfc1a704ab988310345862b294a30cc
Lisbeth Herrera Berrio <lisbeth.herrera@GRUPOBLUMAN.INTRANET>
2024-04-12 20:26:04 -0500

    Se agrega el ejercicio5

---
+++ ejercicio5.js
@@ -0,0 +1,6 @@
+var peso = parseInt(prompt("introduce tu peso en kilogramos"));
+var altura = parseInt(prompt("introduce tu altura en centimetros"));
+
+var IMC = peso / altura**2
+
+alert("tu IMC es= "+IMC)
